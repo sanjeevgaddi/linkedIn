aabd693c455e80a93b8ea3e8e48f909c7a124b8d
sanjeevgaddi <sanjeevgaddi7@gmail.com>
2023-10-03 19:00:09 +0530

    #final commit

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 var profileMenu = document.getElementById("profileMenu");
-      function toggleMenu(){
-        profileMenu.classList.toggle("open-menu")
-      }
+function toggleMenu(){
+  profileMenu.classList.toggle("open-menu")
+}
+
+let sideActivity=document.getElementById("sidebarActivity");
+let moreLink=document.getElementById("showMoreLink");
+
+function toggleActivity()
+{
+  sideActivity.classList.toggle("open-activity");
+
+  if(sideActivity.classList.contains("open-activity"))
+  {
+    moreLink.innerHTML = "show less <b>-</b>"
+  }
+  else{moreLink.innerHTML = "show More <b>+</b>"
+  }
+}
